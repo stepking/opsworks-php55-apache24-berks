532f4bf198d9e291f866ba7e576a4e48f00ab3cc
Stephanie King <stepking@amazon.com>
2014-05-21 14:08:21 -0700

    added provides

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name 'fakecookbook'
+
+provides "opsworks-php55-apache24-berks::setup"
+provides "opsworks-php55-apache24-berks::deploy"
+provides "opsworks-php55-apache24-berks::config"
+provides "opsworks-php55-apache24-berks::default"
+provides "opsworks-php55-apache24-berks::undeploy"
+provides "opsworks-php55-apache24-berks::shutdown"
