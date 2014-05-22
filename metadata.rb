1bc65c5eb221b63b7d2ea86bd8b606b1b6c61a57
stepking <smking3@ncsu.edu>
2014-05-22 09:47:40 -0700

    delete metadata to test berkshelf

--- metadata.rb
+++
@@ -1,8 +0,0 @@
-name 'fakecookbook'
-
-provides "opsworks-php55-apache24-berks::setup"
-provides "opsworks-php55-apache24-berks::deploy"
-provides "opsworks-php55-apache24-berks::config"
-provides "opsworks-php55-apache24-berks::default"
-provides "opsworks-php55-apache24-berks::undeploy"
-provides "opsworks-php55-apache24-berks::shutdown"
